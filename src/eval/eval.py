ea2b7b31be46c9e2f6b0c760c123820361e2afb0
Ákos Virágh <viragh.akos21@gmail.com>
2025-01-04 18:49:32 +0100

    Add combined eval script

---
+++ src/eval/eval.py
@@ -0,0 +1,218 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import os
+import pandas as pd
+import traceback
+
+import utils
+
+from enum import Enum
+
+
+SIM_CONFIG = """
+simulation:
+  path:
+    num_interp_points: 1001
+  velocity:
+    max: __VELOCITY__ # m/s
+motion:
+  sampling: 4 # Hz
+lidar:
+  vlp_16:
+    channels: 16
+    range: 100 # m
+    fov_v: 30 # deg
+    fov_h: 360 # deg
+    ang_res_v: 2 # deg
+    rotation_rate:
+      min: 5 # Hz
+      max: 20 # Hz
+      value: 20 # Hz
+    rpm:
+      min: 300
+      max: 1200
+      partition_size: 60
+      value: 1200
+    firing_cycle: 5.5296e-05
+    accuracy:
+      value: __ACCURACY__ # m
+    random:
+      seed: 1
+"""
+
+PER_CONFIG = """
+random:
+  seed: 1
+downsampling:
+  enabled: true
+  leaf_size:
+    x: 0.07 # m
+    y: 0.07 # m
+    z: 0.07 # m
+plane_filtering:
+  distance_threshold: 0.1 # m
+  min_inliers: 500
+clustering:
+  k_search: 50
+  cluster_size:
+    max: 1000000
+    min: 50
+  num_of_neighbours: 30
+  smoothness_threshold: 0.314159265 # rad (18.f / 180.f * pi)
+  curvature_threshold: 1
+circle_extraction:
+  ransac:
+    distance_threshold: 0.07 # m
+    iter: 10000
+    min_samples: 20
+    r_max: 0.32 # m
+    r_min: 0.28 # m
+pairing:
+  distance_threshold: 0.5 # m
+spatial_consistency:
+  buff_capacity: 3
+  min_occurrence: 2
+  threshold: 0.5
+"""
+
+
+SIM_PARAMS = {
+    "__VELOCITY__": [1, 2, 4],
+    "__ACCURACY__": [0.01, 0.05, 0.1],
+}
+
+PER_PARAMS = {
+
+}
+
+
+class AnsiColors(Enum):
+    DARK_GREY = "\x1b[30;1m"
+    RED       = "\x1b[31;20m"
+    BOLD_RED  = "\x1b[31;1m"
+    GREEN     = "\x1b[32;20m"
+    YELLOW    = "\x1b[33;20m"
+    BLUE      = "\x1b[34;20m"
+    PURPLE    = "\x1b[35;20m"
+    CYAN      = "\x1b[36;20m"
+    WHITE     = "\x1b[37;1m"
+    GREY      = "\x1b[38;20m"
+    DEFAULT   = "\x1b[0m"
+
+
+class ColorLogFormatter(logging.Formatter):
+    formats = {
+        logging.DEBUG:    "%(asctime)s [%(name)s] {}%(levelname)s:{} %(message)s".format(AnsiColors.BLUE.value, AnsiColors.DEFAULT.value),
+        logging.INFO:     "%(asctime)s [%(name)s] {}%(levelname)s:{} %(message)s".format(AnsiColors.GREEN.value, AnsiColors.DEFAULT.value),
+        logging.WARNING:  "%(asctime)s [%(name)s] {}%(levelname)s:{} %(message)s".format(AnsiColors.YELLOW.value, AnsiColors.DEFAULT.value),
+        logging.ERROR:    "%(asctime)s [%(name)s] {}%(levelname)s:{} %(message)s".format(AnsiColors.RED.value, AnsiColors.DEFAULT.value),
+        logging.CRITICAL: "%(asctime)s [%(name)s] {}%(levelname)s:{} %(message)s".format(AnsiColors.BOLD_RED.value, AnsiColors.DEFAULT.value)
+    }
+
+    def format(self, record):
+        log_fmt = self.formats.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
+log_handler = logging.StreamHandler()
+log_handler.setFormatter(ColorLogFormatter())
+
+logger = logging.getLogger("Eval")
+logger.addHandler(log_handler)
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="Evaluation script",
+    )
+
+    parser.add_argument("-s", "--simulator", metavar="SIMULATOR_BIN", type=str, required=True, help="path of the simulator binary executable")
+    parser.add_argument("-p", "--perception", metavar="PERCEPTION_BIN", type=str, required=True, help="path of the perception binary executable")
+    parser.add_argument("-w", "--work-dir", type=str, required=True, help="path of the working directory (will be created if not present)")
+    parser.add_argument("--map", type=str, required=True, help="path of the map file")
+    parser.add_argument("--path", type=str, required=True, help="path of the path file")
+    parser.add_argument("-v", "--verbose", action="store_true")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+
+    if os.path.exists(args.work_dir) and not os.path.isdir(args.work_dir):
+        logger.error(f"The path `{args.work_dir}` already exists and it is not a directory!")
+        exit(1)
+
+    if not os.path.exists(args.work_dir):
+        os.makedirs(args.work_dir)
+        logger.info(f"The directory `{args.work_dir}` has been created!")
+
+    logger.info(f"Simulator: {args.simulator}")
+    logger.info(f"Perception: {args.perception}")
+    logger.info(f"Working directory: {args.work_dir}\n")
+
+    sim_out_dirs = utils.run_simulator(args.simulator, args.work_dir, args.map, args.path, SIM_CONFIG, SIM_PARAMS, logger)
+    logger.debug(f"Simulator output directories: {sim_out_dirs}")
+
+    per_out_dirs = utils.run_perception(args.perception, args.work_dir, sim_out_dirs, PER_CONFIG, PER_PARAMS, logger)
+    logger.debug(f"Perception output directories: {per_out_dirs}")
+
+    ref_points = utils.read_ref_points(args.map)
+    logger.debug(f"Reference points: {ref_points}")
+
+    path_points = utils.read_path_points(args.path)
+    logger.debug(f"Path points: {path_points}")
+
+    origin = path_points[0]
+
+    dist_sum = lambda l: sum((r - p).length() for r, p in l)
+
+    evaluated_scenarios = {}
+
+    for dir in per_out_dirs:
+        file_path_list, paired_list, out_of_range_list = utils.eval_scenario(f"{dir}/data", origin, ref_points, logger)
+        evaluated_scenarios[dir] = (file_path_list, paired_list, out_of_range_list)
+
+    for k, v in evaluated_scenarios.items():
+        data = {
+            "file": [],
+            "success_ratio": [],
+            "fail_ratio": [],
+            "avg_in_range_dist": [],
+            "avg_out_of_range_dist": [],
+        }
+
+        for file, paired, out_of_range in zip(*v):
+            data["file"].append(file)
+            if len(paired) > 0 or len(out_of_range) > 0:
+                data["success_ratio"].append(len(paired) / (len(paired) + len(out_of_range)))
+                data["fail_ratio"].append(len(out_of_range) / (len(paired) + len(out_of_range)))
+            else:
+                data["success_ratio"].append("NaN")
+                data["fail_ratio"].append("NaN")
+            if len(paired) > 0:
+                data["avg_in_range_dist"].append(dist_sum(paired) / len(paired))
+            else:
+                data["avg_in_range_dist"].append("NaN")
+            if len(out_of_range) > 0:
+                data["avg_out_of_range_dist"].append(dist_sum(out_of_range) / len(out_of_range))
+            else:
+                data["avg_out_of_range_dist"].append("NaN")
+        df = pd.DataFrame(data)
+        df.to_excel(f"{k}.xlsx", index=False)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as ex:
+        logger.error(f"Exception: {ex}")
+        if logger.getEffectiveLevel() == logging.DEBUG:
+            traceback.print_exception(ex)
---
+++ src/eval/utils.py
@@ -0,0 +1,222 @@
+import fnmatch
+import itertools
+import math
+import os
+import re
+import shlex
+import subprocess
+
+from typing import List, Tuple
+
+from vec import Vec2f
+
+
+def eval_file(file_path: str, ref: List[Vec2f], origin: Vec2f, threshold: float):
+    with open(file_path, "r") as inf:
+        data = inf.readlines()
+
+    points = []
+
+    for line in data:
+        p = line.strip().split()
+        points.append(Vec2f(float(p[0]), float(p[1])) + origin)
+
+    paired = pair_with_ref(ref, points, threshold)
+    out_of_range = points_out_of_range(ref, points, threshold)
+
+    return paired, out_of_range
+
+
+def eval_scenario(dir, origin, ref_points, logger):
+    file_path_list = files(dir, "registered-*")
+    file_path_list.sort(key=lambda x: int(re.sub(".*registered-", "", x).rstrip(".xyz")))
+    logger.debug(f"Files to process: {file_path_list}")
+
+    indices = []
+    paired_list = []
+    out_of_range_list = []
+
+    for file_path in file_path_list:
+        logger.info(f"Processing file: {file_path}")
+        paired, out_of_range = eval_file(file_path, ref_points, origin, threshold=0.5)
+        paired_list.append(paired)
+        out_of_range_list.append(out_of_range)
+
+    return file_path_list, paired_list, out_of_range_list
+
+
+def files(folder: str, pattern: str = "*") -> list:
+    # List all files and directories in the specified path
+    entries = os.listdir(folder)
+
+    # Filter out directories if you only want files
+    files = [
+        os.path.realpath(os.path.join(folder, f))
+        for f in entries
+        if os.path.isfile(os.path.join(folder, f)) and fnmatch.fnmatch(f, pattern)
+    ]
+
+    return files
+
+
+def generate_configs(s: str, d: dict):
+    def generate_all_combinations(d: dict) -> list:
+        ret = []
+
+        cartesian_prod = list(itertools.product(*d.values()))
+
+        for elem in cartesian_prod:
+            dd = dict(zip(d.keys(), elem))
+            ret.append(dd)
+
+        return ret
+
+    ret = []
+    cfgs = generate_all_combinations(d)
+
+    for cfg in cfgs:
+        name = ""
+        ss = s
+        for k, v in cfg.items():
+            name += f"_{k.lower().strip('__')}_{str(v)}"
+            ss = ss.replace(k, str(v), -1)
+        name = name[1:]
+        ret.append((name, ss))
+
+    return ret
+
+
+def pair_with_ref(ref: List[Vec2f], points: List[Vec2f], threshold: float) -> List[Tuple[Vec2f, Vec2f]]:
+    """
+    Pair each point with reference points if they are within the threshold distance.
+
+    :param ref: List of reference points.
+    :param points: List of points to be paired.
+    :param threshold: Maximum distance for pairing.
+    :return: List of tuples where each tuple contains a reference point paired with a point from 'points'.
+    """
+    paired = []
+
+    for r in ref:
+        for p in points:
+            dist = (r - p).length()
+            if dist <= threshold:
+                paired.append((r, p))
+
+    return paired
+
+
+def points_out_of_range(ref: List[Vec2f], points: List[Vec2f], threshold: float) -> List[Tuple[Vec2f, float]]:
+    ret = []
+
+    for p in points:
+        out = True
+        min_dist = float(math.inf)
+        closest_r = None
+        for r in ref:
+            dist = (r - p).length()
+            if dist <= threshold:
+                out = False
+            elif dist <= min_dist:
+                min_dist = dist
+                closest_r = r
+        if out:
+            ret.append((closest_r, p))
+
+    return ret
+
+
+def read_ref_points(file_path: str):
+    ret = []
+
+    with open(file_path, "r") as inf:
+        data = inf.readlines()
+
+    data = list(filter(lambda x: x.lstrip().startswith("cylinder"), data))
+
+    for d in data:
+        d = d.split()
+        ret.append(Vec2f(float(d[1]), float(d[2])))
+
+    return ret
+
+
+def read_path_points(file_path: str):
+    ret = []
+
+    with open(file_path, "r") as inf:
+        data = inf.readlines()
+
+    for d in data:
+        d = d.split()
+        ret.append(Vec2f(float(d[0]), float(d[1])))
+
+    return ret
+
+
+def run_simulator(sim_bin, work_dir, map, path, sim_cfg, sim_params, logger):
+    sim_cfgs = generate_configs(sim_cfg, sim_params)
+    sim_out_dirs = []
+
+    for name, cfg in sim_cfgs:
+        out_dir = f"{work_dir}/sim_out-{name}"
+
+        if not os.path.exists(f"{out_dir}/data"):
+            os.makedirs(f"{out_dir}/data")
+
+        with open(f"{out_dir}/sim.cfg.yaml", "w") as of:
+            of.write(cfg.strip())
+
+        sim_out_dirs.append(out_dir)
+        logger.info(f"[Simulator] Output directory: {out_dir}")
+
+        result = run_command(f"{sim_bin} --config {out_dir}/sim.cfg.yaml --map {map} --path {path} --outdir {out_dir}/data")
+        if result.returncode != 0:
+            logger.error("[Simulator] Something went wrong!")
+            logger.error(f"[Simulator] STDOUT:\n{result.stdout}")
+            logger.error(f"[Simulator] STDERR:\n{result.stderr}")
+        break   # TODO: Remove
+
+    return sim_out_dirs
+
+
+def run_perception(per_bin, work_dir, sim_out_dirs, per_cfg, per_params, logger):
+    per_cfgs = generate_configs(per_cfg, per_params)
+    per_out_dirs = []
+
+    for od in sim_out_dirs:
+        end = len(files(f"{od}/data", "test_fn*"))
+
+        for name, cfg in per_cfgs:
+            out_dir = f"{work_dir}/per_out-{od.lstrip(f'{work_dir}/')}-{name}"
+
+            if not os.path.exists(f"{out_dir}/data"):
+                os.makedirs(f"{out_dir}/data")
+
+            with open(f"{out_dir}/per.cfg.yaml", "w") as of:
+                of.write(cfg.strip())
+
+            per_out_dirs.append(out_dir)
+            logger.info(f"[Perception] Output directory: {out_dir}")
+
+            result = run_command(f"{per_bin} --config {out_dir}/per.cfg.yaml --indir {od}/data --start 1 --end {end} --outdir {out_dir}/data --format xyz")
+            if result.returncode != 0:
+                logger.error("[Perception] Something went wrong!")
+                logger.error(f"[Perception] STDOUT:\n{result.stdout}")
+                logger.error(f"[Perception] STDERR:\n{result.stderr}")
+            break   # TODO: Remove
+        break   # TODO: Remove
+
+    return per_out_dirs
+
+
+def run_command(command: str, shell=False) -> int:
+    """ Run a command
+
+    Raises exception if the `command` is not valid.
+    """
+
+    if command in ["", None]:
+        raise Exception("Command cannot be empty or `None`")
+
+    return subprocess.run(shlex.split(command), capture_output=True, shell=shell)
